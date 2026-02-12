7f918692ae986d0f1f4a959bb9211bcd970727ab
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2026-02-13 00:20:03 +0500

    added test api

--- apps/kiosk-backend/app/app.py
+++ apps/kiosk-backend/app/app.py
@@ -64,6 +64,9 @@ def create_app() -> FastAPI:
 
 app = create_app()
 
+@app.get("/")
+def response():
+  return {"Salam from Event Kiosk, how can I help you today?"}
 
 @app.get("/api/version")
 def version():
